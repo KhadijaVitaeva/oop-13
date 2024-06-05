fde7a41f46d4ac62b2161ad557f127b49487d5b3
KhadijaVitaeva <jamalvit9595@gmail.com>
2024-06-06 01:43:23 +0300

    initial4

--- classes/category.py
+++ classes/category.py
@@ -1,6 +1,5 @@
 from classes.product import Product
 
-
 class Category:
     category_name: str
     description_category: str
@@ -10,26 +9,19 @@ class Category:
 
     def __init__(self, category_name, description_category, category_products):
         self.category_name = category_name
-
         self.description_category = description_category
-
         self.__category_products = category_products
-
         Category.category_count += 1
-
         Category.product_count += len(self.__category_products)
 
     def __repr__(self):
         return (f"Имя категории - {self.category_name}; "
-
                 f"Описание категории - {self.description_category}; Список продуктов - {self.__category_products}\n")
 
     def __len__(self):
         len_products = 0
-
         for i in self.__category_products:
             len_products += i.quantity_in_stock
-
         return len_products
 
     def __str__(self):
@@ -50,40 +42,26 @@ class Category:
             print("Продукт должен быть объектом класса Product")
 
     def average_price(self):
-
         """
-
         метод, который подсчитывает средний ценник всех товаров.
-
         :return:
-
         """
-
         try:
-
             result = 0
-
             for product in self.__category_products:
                 result += product.price * product.quantity_in_stock
-
             return f'Средний чек {result / len(self)}'
-
         except ZeroDivisionError:
-
             print('0')
 
     def get_product_count(self):
-
         return len(self.__category_products)
 
     @property
     def category_products(self):
-
         result = ''
-
         for product in self.__category_products:
             result += f'{product.product_name}, {product.price} руб. Остаток: {product.quantity_in_stock} шт.\n'
-
         return result
 
 # class CategoryIter:
--- classes/product.py
+++ classes/product.py
@@ -1,25 +1,36 @@
-class Product:
+from abc import ABC, abstractmethod
+
+class BaseClass(ABC):
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+class MixinLog:
+    def __init__(self, *args, **kwargs):
+        print(repr(self))
+    def __repr__(self):
+        return f'{self.__class__.__name__}, {self.__dict__.items()}'
+
+class Product(MixinLog, BaseClass):
     product_name: str
     product_description: str
     price: float
     quantity_in_stock: int
-
+    color = str
     def __init__(self, product_name, product_description, price, quantity_in_stock):
         self.product_name = product_name
         self.product_description = product_description
         self.__price = price
         self.quantity_in_stock = quantity_in_stock
-
+        super().__init__()
     def __repr__(self):
         return (f"Имя продукта - {self.product_name} Описание продукта - {self.product_description} "
                 f"Стоимость продукта - {self.__price} Количество - {self.quantity_in_stock}")
-
     def __str__(self):
         return f'{self.product_name}, {self.__price} руб. Остаток: {self.quantity_in_stock} шт.\n'
-
     def __add__(self, other):
         """
-        сложение двух продуктов - сложением сумм, умноженных на количество на складе.
+        слоение двух продуктов - сложением сумм, умноженных на количество на складе.
         :param other:
         :return:
         """
@@ -29,7 +40,6 @@ class Product:
             raise TypeError
     @staticmethod
     def my_decorator(func):
-
         def inner(**kwargs):
             result = func(**kwargs)
             result2 = {
@@ -40,7 +50,6 @@ class Product:
             }
             return result2
         return inner
-
     @classmethod
     def create_new_product_object(cls, **kwargs):
         """
@@ -51,7 +60,6 @@ class Product:
         price = kwargs['price']
         quantity_in_stock = kwargs['quantity_in_stock']
         return cls(product_name, product_description, price, quantity_in_stock)
-
     @property
     def price(self):
         return self.__price
@@ -62,3 +70,22 @@ class Product:
         else:
             self.__price = new_price
             print("цена корректная")
+class SmartPhone(Product, MixinLog):
+    productivity = str
+    model = str
+    internal_memory = float
+    color = str
+    def __init__(self, productivity, model, internal_memory, color, product_name, product_description, price,
+                 quantity_in_stock):
+        super().__init__(product_name, product_description, price, quantity_in_stock)
+        self.productivity = productivity
+        self.model = model
+        self.internal_memory = internal_memory
+        self.color = color
+class LawnGrass(Product, MixinLog):
+    def __init__(self, manufacturer_country, germination_period, color, product_name, product_description, price,
+                 quantity_in_stock):
+        super().__init__(product_name, product_description, price, quantity_in_stock)
+        self.manufacturer_country = manufacturer_country
+        self.germination_period = germination_period
+        self.color = color
